58b7cd46b5fd738ab55be901afa6d1fc21b29492
fire8811 <jensonjones2002@gmail.com>
2025-02-14 00:37:33 -0700

    Add main section of Home page

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import '../app.css';
 
 export function Home(){
     return (
         <main className="container-fluid bg-secondary text-center">
-            <div>Placeholder Home</div>
+            <h2> Welcome to Huey</h2>
+
+            <div id="card" className="demo-box">
+            <div className="card border-0 shadow" style={{width:"400px"}}>
+                <div className="card-body">
+                    <div className="form-group">
+                        <input type="text" className="form-control" id="username" placeholder="Username"/>
+                    </div>
+
+                    <div className="form-group">
+                        <input type="text" className="form-control" id="password" placeholder="Password"/>
+                    </div>
+                    <div id="btn">
+                        <a href="#" className="btn btn-primary">Login</a>
+                        <a href="#" className="btn btn-outline-primary">Sign up</a>
+                    </div>
+                
+                </div>
+            </div>
+            </div>
+            <form method="get" action="play.html"></form>
         </main>
     );
 }
