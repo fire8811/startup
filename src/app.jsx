82856e1679c7beb9082cd3f06d859bb2f36d197f
fire8811 <jensonjones2002@gmail.com>
2025-02-13 23:53:11 -0700

    Add header and footer to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App(){
-    return <div className="body bg-dark text-light">Placeholder for main</div>
+    return(
+    <div className="body bg-dark text-dark"> 
+      
+    <header className ="container-fluid">
+      <nav className="navbar fixed-top navbar-dark">
+        <a className="navbar-brand" href="#">
+          <img alt="Paint" src="Assets/pallette.png" width="50" />
+          Huey!<sup>&TRADE;</sup></a>
+        <menu className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link active" href="index.html">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" href="play.html">Play</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" href="instructions.html">Instructions</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" href="leaderboard.html">Leaderboard</a>
+          </li>
+        </menu>
+      </nav>
+    </header>
+
+    <main>
+      <h2> Welcome to Huey</h2>
+    </main>
+
+
+    <footer className="text-white">
+      <div className="container-fluid">
+        <span className="text-reset"> &copy;2025 Jenson J. </span>
+        <a href = "https://github.com/fire8811/startup.git">(GitHub)</a>
+        <a className="pallete-credit" href="https://www.vecteezy.com/free-png/paint-palette">Paint Palette PNGs by Vecteezy</a>
+      </div>
+    </footer>
+    </div>
+    );
 }
