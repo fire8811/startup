cc87fe53fd9d3d8c240492d440d197d130056d59
fire8811 <jensonjones2002@gmail.com>
2025-02-21 13:18:20 -0700

    Add jsx for game component with its framework

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Game} from './game'
 import './play.css'
 export function Play(){
     return (
@@ -11,45 +12,7 @@ export function Play(){
           </ul>
         </div>
 
-        <div className="center-content">
-          <h3>Player_Name's Game</h3>
-          <div id="card" className="demo-box">
-            <div className="card border-0 shadow" style={{width:"400px"}}>
-              <div className="card-body">
-                <div className="game-info">
-                  <div className="score">
-                    Score: &nbsp;<span id="score-value">00</span>
-                  </div>
-                  <div className="time">
-                    Time Remaining: &nbsp;<span id="time-remaining">60s</span>
-                  </div>
-                </div>
-                <h5> [API PROVIDED COLOR NAME HERE]</h5>
-                <div className="squares">
-                  <svg width="100" height="100">
-                    <rect width="100" height="100" fill="red"/>
-                  </svg>
-                  <svg width="100" height="100">
-                    <rect width="100" height="100" fill="blue"/>
-                  </svg>
-                </div>
-
-                
-                  <label for="color">Pick Color: </label>
-                <div id="color-picker">
-                  <input type="color" value="#ff350c" name="varColor" id="color" />
-                </div>
-                
-                <div id="btn">
-                  <a href="#" className="btn btn-success">Select</a>
-                  <a href="#" className="btn btn-secondary">Pause</a>
-                  <a href="#" className="btn btn-danger">Reset</a>
-                </div>
-                
-              </div>
-            </div>
-          </div>
-        </div>
+        <Game/>
         </main>
     );
 }
