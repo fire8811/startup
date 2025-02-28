af24db33d09842a53568d45c9f0a5ffc1ffc6ec6
fire8811 <jensonjones2002@gmail.com>
2025-02-27 19:15:01 -0700

    add initial setup for GG notifications

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -4,15 +4,27 @@ import './play.css'
 export function Play({user}){
     return (
         <main className="container-fluid bg-secondary text-center">
-        <div className="notification text-start">
-          <ul>
-            <li className="player-name"> Calvin started a new game</li>
-            <li className="player-name"> Hobbes scored 7</li>
-            <li className="player-name"> Hobbes beat his high score!</li>
-          </ul>
-        </div>
+          <div class="row-container">
+            
+            <div className="notification text-start">
+              <ul>
+                <li className="player-name"> Calvin started a new game</li>
+                <li className="player-name"> Hobbes scored 7</li>
+                <li className="player-name"> Hobbes beat his high score!</li>
+              </ul>
+            </div>
+          
+            <div id="btn">
+              <a href="#" className="btn btn-primary">GG</a>
+            </div>
+        
+          </div>
+        
+        
 
         <Game user={user}/>
+        
+        
         </main>
     );
 }
