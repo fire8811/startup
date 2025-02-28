acb71a696ba07c5f6290a1efd64daad6f6613c92
fire8811 <jensonjones2002@gmail.com>
2025-02-27 18:51:10 -0700

    add leaderboard table

--- src/leaderboard/leaderboard.jsx
+++ src/leaderboard/leaderboard.jsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import './leaderboard.css'
+import './leaderboard.css';
 
 export function Leaderboard(){
+  const [scores, setScores] = React.useState([]);
+
+  React.useEffect(()=>{
+    const scoresObj = localStorage.getItem('scores');
+    if (scoresObj){
+      setScores(JSON.parse(scoresObj));
+    }
+  }, []);
+
+  const rows = [];
+  if (scores.length){
+    for(const [i, score] of scores.entries()) {
+      rows.push(
+        <tr key={i}>
+          <td>{i+1}</td>
+          <td>{score.name}</td>
+          <td>{score.score}</td>
+          <td>{score.time}</td>
+        </tr>
+      );
+    }
+  } else {
+    <tr key='1'>
+      <td colSpan='4'>Get First Place For Free</td>
+    </tr>
+  }
+
     return (
-        <main className="container-fluid bg-secondary text-center">
-            <div>
-        <h3> Top Scores </h3>
-        <ol>
-          <li>
-            <div class="score-name">Spaceman Spiff</div>
-            <div class="score">15</div>
-          </li>
-          <li>
-            <div class="score-name">Miss Wormwood</div>
-            <div class="score">12</div>
-          </li>
-          <li>
-            <div class="score-name">Susie Derkins</div>
-            <div class="score">9</div>
-          </li>
-        </ol>
-      </div>
+        <main className="container-fluid bg-secondary text-center p-5">  
+          <h3> Top Scores </h3>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Place</th>
+                <th>Name</th>
+                <th>Score</th>
+                <th>How Late</th>
+              </tr>
+            </thead>
+            <tbody id='scores'>{rows}</tbody>
+          </table>
         </main>
     );
 }
