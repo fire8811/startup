5089ad945f971609d7d0803be502e8bf84c3296a
fire8811 <jensonjones2002@gmail.com>
2025-02-27 20:23:46 -0700

    working websocket simulation

--- src/play/gg.jsx
+++ src/play/gg.jsx
@@ -4,19 +4,38 @@ import './play.css';
 export function GGnotification({user}){
     const username = user;
     const [msg, setMsg] = React.useState('...listening');
+    const verbs = [
+        "said", "stated", "uttered", "voiced", "expressed", "declared", "remarked",  
+        "noted", "mentioned", "added", "commented", "murmured", "whispered", "muttered",  
+        "mumbled", "hissed", "sighed", "grumbled", "groaned", "breathed", "exclaimed",  
+        "shouted", "yelled", "screamed", "shrieked", "howled", "roared", "barked",  
+        "thundered", "bellowed", "wailed", "cried", "gasped", "blurted",  
+        "snapped", "snarled", "demanded", "argued", "protested", "insisted",  
+        "countered", "interjected", "inquired", "questioned", 
+        "cheered", "gushed", "chimed", "beamed", "laughed", "grinned", "encouraged",  
+        "congratulated", "enthused", "exclaimed", 
+        "joked", "teased", "chirped", "sang", "warbled", "trilled", "bubbled",  
+        "radiated", "applauded", "hailed", "exulted", "rejoiced", "proclaimed", "boasted",  
+        "bragged", "chuckled", "giggled", "snickered", "smiled", 
+         "exclaimed joyfully", "exuberated", "whooped", 
+        "squealed", 
+      ];
 
     React.useEffect(() => {
         setInterval(() => {
             const users = ['Calvin', 'Hobbes', 'SpacemanSpiff', 'SusieDerkins', 'TracerBullet'];
+            
             const randomUser = users[Math.floor(Math.random() * users.length)];
-            const newMsg = `${randomUser} said GG`;
+            const randomVerb = verbs[Math.floor(Math.random() * verbs.length)];
+            const newMsg = `${randomUser} ${randomVerb} GG`;
             setMsg(newMsg);
 
-        }, 5000);
+        }, 2000);
     })
 
     function ggClick(){
-        setMsg(`${username} said GG`)
+        const randomVerb = verbs[Math.floor(Math.random() * verbs.length)];
+        setMsg(`${username} ${randomVerb} GG`)
     }
 
     
