efa3329cf675274ccc8101d67a22136412807d16
fire8811 <jensonjones2002@gmail.com>
2025-03-17 17:06:11 -0600

    Initial set up for endpoints

--- index.js
+++ index.js
@@ -16,8 +16,38 @@ app.use(express.static('public'));
 var router = express.Router();
 app.use('/api', apiRouter);
 
+async function createUser(email, password) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = {
+        email: email,
+        password: hashedPassword,
+        token = user.token = uuid.v4();
+    };
+
+    users.push(newUser);
+    return newUser;
+}
+
+function setAuthCookie(res, user) {
+    res.cookie('token', user.token, {
+        secure: true,
+        httpOnly: true,
+        sameSite: 'strict'
+    });
+}
+
+function getUser(field, value){
+    if (value) {
+        return users.find((user) => user[field] === value);
+    }
+    return null;
+}
+
+//registration
+app.post('api/auth')
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 app.listen(port, () => {
     console.log("running");
-})
+});
+
