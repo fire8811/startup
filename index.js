c1b45106f4549f2995fd484df8e3ed2ab18c0262
fire8811 <jensonjones2002@gmail.com>
2025-03-17 19:40:35 -0600

    Add create user and login functionality

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const cookieParser = require('cookie-parser');
-const crypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const express = require('express');
 const uuid = require('uuid');
 const app = express();
@@ -14,14 +14,14 @@ app.use(cookieParser());
 app.use(express.static('public'));
 
 var router = express.Router();
-app.use('/api', apiRouter);
+app.use(`/api`, router);
 
 async function createUser(email, password) {
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = {
         email: email,
         password: hashedPassword,
-        token = user.token = uuid.v4();
+        token: uuid.v4(),
     };
 
     users.push(newUser);
@@ -44,10 +44,40 @@ function getUser(field, value){
 }
 
 //registration
-app.post('api/auth')
+router.post('/create', async (req, res) => {
+    if (await getUser('email', req.body.email)) {  //check to see if username/email is avaliable 
+        res.status(409).send({ msg: 'Username Taken!'});
+    } else {
+        const newUser = await createUser(req.body.email, req.body.password);
+
+        setAuthCookie(res, newUser.token);
+        res.send({ email: newUser.email });
+    }
+});
+
+//login
+router.post('/login', async (req, res) => {
+    const user = await getUser('email', req.body.email);
+
+    if (user && (await bcrypt.compare(req.body.password, user.password))) { //check if user exists AND passwords match
+        user.token = uuid.v4();
+        setAuthCookie(res, user);
+        res.send( {email: user.email});
+        return;
+
+    } else {
+        res.status(401).send({msg: 'Unauthorized' });
+    }
+})
+
+//return scores
+router.get('/scores', (_req, res) => {
+    res.send(scores);
+})
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
 app.listen(port, () => {
-    console.log("running");
+    console.log(`running on port ${port}`);
 });
 
--- src/home/unauthenticated.jsx
+++ src/home/unauthenticated.jsx
@@ -4,18 +4,40 @@ import '../app.css';
 export function Unauthenticated(props) {
     const[username, setUsername] = React.useState(props.username);
     const [password, setPassword] = React.useState('');
+    const [displayError, setDisplayError] = React.useState(null);
 
-    async function login() {
-        localStorage.setItem('USERNAME', username);
-        props.onLogin(username);
+    function login() {
+        // localStorage.setItem('USERNAME', username);
+        // props.onLogin(username);
+        authenticate('api/login')
     }
 
-    async function createUser() {
-        localStorage.setItem('USERNAME', username);
-        props.onLogin(username);
+    function createUser() {
+        // localStorage.setItem('USERNAME', username);
+        // props.onLogin(username);
+        authenticate('api/create')
+    }
+
+    async function authenticate(endpoint){
+        const response = await fetch(endpoint, {
+            method: 'post',
+            body: JSON.stringify({email: username, password: password }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+
+        if (response?.status === 200){
+            localStorage.setItem('USERNAME', username);
+            props.onLogin(username);
+        } else {
+            const body = await response.json();
+            console.log(`ERRROR: ${body.msg}`);
+        }
     }
     
     return (
+        <>
         <div id="card" className="demo-box">
             <div className="card border-0 shadow" style={{width:"400px"}}>
                 <div className="card-body">
@@ -34,5 +56,6 @@ export function Unauthenticated(props) {
                 </div>
             </div>
         </div>
+        </>
     );
 }
