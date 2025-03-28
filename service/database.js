fd48096144d56d3d38e81a140f5cd1661d5dbb2b
fire8811 <jensonjones2002@gmail.com>
2025-03-28 12:23:28 -0600

    add database connection

---
+++ service/database.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.username}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('huey');
+const userCollection = db.collection('user')
+const scoreCollection = db.collection('score');
+
+async function testConnection() {
+    try {
+        await db.command({ping: 1});
+        console.log('connected to database');
+    } catch (e) {
+        console.log(`error when connecting to database: ${e.message}`);
+        process.exit(1);
+    }
+}
+
+testConnection();
