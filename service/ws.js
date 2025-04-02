e3f1822924dcd594c693b10be7166f489f20d1c0
fire8811 <jensonjones2002@gmail.com>
2025-04-02 10:53:43 -0600

    add websocket send/pin/pong functionality

--- service/ws.js
+++ service/ws.js
@@ -1,1 +1,35 @@
 const { WebSocketServer } = require('ws');
+
+function webSocket(httpServer){
+    const socketServer = new WebSocketServer({ server: httpServer });
+
+    socketServer.on('connection', (socket) => {
+        socket.isAlive = true;
+
+        //forward messages to all users (including sender)
+        socket.on('message', function message(data) {
+            socketServer.clients.forEach((client) => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(data);
+                }
+            });
+        });
+
+        //pong response
+        socket.on('pong', () => {
+            socket.isAlive = true;
+        });
+    });
+
+    //ping sender
+    setInterval(() => {
+        socketServer.clients.forEach(function each(client) {
+            if (client.isAlive === false) return client.terminate;
+
+            client.isAlive = false;
+            client.ping();
+        });
+    }, 10000);
+}
+
+module.exports = { webSocket };
